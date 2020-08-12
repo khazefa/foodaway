bafc8e4fc663272695a0544c3ec40b97f70d76f6
Sigit Prayitno <sigit.prayitno@batmandiri.com>
2020-08-12 22:32:11 +0700

    Modify Main Class

--- src/main/java/id/digifreneur/foodaway/FoodawayApplication.java
+++ src/main/java/id/digifreneur/foodaway/FoodawayApplication.java
@@ -2,16 +2,21 @@ package id.digifreneur.foodaway;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class FoodawayApplication {
+
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodawayApplication.class, args);
@@ -21,7 +26,8 @@ public class FoodawayApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String appName = env.getProperty("app.name");
+			System.out.println("Let's inspect the beans in " + appName + " provided by Spring Boot:");
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
